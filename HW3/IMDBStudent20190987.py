3a9d541d39191acb8ebb53b3b0765633b30c31e9
alswn3844 <alswn3844@naver.com>
2022-10-30 00:11:50 +0900

    add hw3 code

---
+++ HW3/IMDBStudent20190987.py
@@ -0,0 +1,25 @@
+import sys
+inputFile = str(sys.argv[1])
+outputFile = str(sys.argv[2])
+genreList = {}
+
+f = open(outputFile, "at")
+with open(inputFile,"rt") as fp:
+	for line in fp:
+		movie = line.split('::')
+		if movie[2].find('|'):
+			genre = movie[2].split('|')
+			for i in genre:
+				if i.strip() in genreList:
+					genreList[i.strip()] = genreList[i.strip()]+1
+				else:
+					genreList[i.strip()] = 1
+		else:
+			genre = movie[2].strip()
+			if genre in genreList:
+				genreList[genre] = genreList[genre]+1
+			else:
+				genreList[genre] = 1
+keylist = genreList.keys()
+for i in keylist:
+	f.write("%s %d\n" % (i, genreList[i]))
